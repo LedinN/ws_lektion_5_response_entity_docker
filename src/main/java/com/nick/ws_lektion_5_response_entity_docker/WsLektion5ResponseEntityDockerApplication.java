c96285c65fe226f790cc55cca5b6ae40bcb83dd2
Nick <nick.ledin@hotmail.com>
2024-09-06 11:43:30 +0200

    first commit

---
+++ src/main/java/com/nick/ws_lektion_5_response_entity_docker/WsLektion5ResponseEntityDockerApplication.java
@@ -0,0 +1,13 @@
+package com.nick.ws_lektion_5_response_entity_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsLektion5ResponseEntityDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsLektion5ResponseEntityDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nick/ws_lektion_5_response_entity_docker/WsLektion5ResponseEntityDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick.ws_lektion_5_response_entity_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsLektion5ResponseEntityDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
